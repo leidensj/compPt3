603a6fade2fa0d7b56b8d8611b6aaa5803d0a562
gsrodrigues <gennaro.rodrigues@gmail.com>
2014-04-23 20:16:48 -0300

    funcao comentada arrumada

--- astree.c
+++ astree.c
@@ -319,109 +319,109 @@ void astPrintTree(ASTREE* root, int level)
      switch(node->type)
      {
           case AST_GLOBAL_DECL:
-               fprintf(fd, "GLOBAL_DECL, ");
+               printf(fd, "GLOBAL_DECL, ");
           case AST_SYMBOL:
-               fprintf(fd, "SYMBOL, ");break;               
+               printf(fd, "SYMBOL, ");break;               
           case AST_ADD:
-               fprintf(fd, "ADD, ");break;
+               printf(fd, "ADD, ");break;
           case AST_SUB:
-               fprintf(fd, "SUB, ");break; 
+               printf(fd, "SUB, ");break; 
           case AST_MUL:
-               fprintf(fd, "MUL, ");break; 
+               printf(fd, "MUL, ");break; 
           case AST_DIV:
-               fprintf(fd, "DIV, ");break;
+               printf(fd, "DIV, ");break;
           case AST_EQUALS:
-               fprintf(fd, "EQUALS, ");break;
+               printf(fd, "EQUALS, ");break;
           case AST_CMDS:
-               fprintf(fd, "CMDS, ");break;
+               printf(fd, "CMDS, ");break;
           case AST_KW_LOOP:
-               fprintf(fd, "LOOP, ");break;
+               printf(fd, "LOOP, ");break;
           case AST_KW_IF:
-               fprintf(fd, "IFTHEN, ");break;
+               printf(fd, "IFTHEN, ");break;
           case AST_KW_IF_ELSE:
-               fprintf(fd, "IFELSE, ");break;
+               printf(fd, "IFELSE, ");break;
           case AST_KW_INPUT:
-               fprintf(fd, "INPUT, ");break;
+               printf(fd, "INPUT, ");break;
           case AST_OUTPUT:
-               fprintf(fd, "OUTPUT, ");break;
+               printf(fd, "OUTPUT, ");break;
           case AST_RETURN:
-               fprintf(fd, "RETURN, ");break;
+               printf(fd, "RETURN, ");break;
           case AST_FUNCTION_DECL:
-               fprintf(fd, "FUNCTIONDECL, ");break;
+               printf(fd, "FUNCTIONDECL, ");break;
           case AST_DECL_VECTOR:
-               fprintf(fd, "VECDECL, ");break;
+               printf(fd, "VECDECL, ");break;
           case AST_DECL_VECTOR_INITVALUE:
-               fprintf(fd, "VECDECLINIT, ");break;
+               printf(fd, "VECDECLINIT, ");break;
           case AST_DECL:
-               fprintf(fd, "VARDECL, ");break;
+               printf(fd, "VARDECL, ");break;
           case AST_DECL_POINTER:
-               fprintf(fd, "DECLPOINTER, ");break;
+               printf(fd, "DECLPOINTER, ");break;
           case AST_VECTOR_SIZE:
-               fprintf(fd, "VECTOR_SIZE, ");break;
+               printf(fd, "VECTOR_SIZE, ");break;
           case AST_LIT_INTEGER:
-               fprintf(fd, "LITERAL_INTEGER, ");break;
+               printf(fd, "LITERAL_INTEGER, ");break;
           case AST_LIT_TRUE:
-               fprintf(fd, "LITERAL_TRUE, ");break;
+               printf(fd, "LITERAL_TRUE, ");break;
           case AST_LIT_FALSE:
-               fprintf(fd, "LITERAL_FALSE, ");break;
+               printf(fd, "LITERAL_FALSE, ");break;
           case AST_LIT_CHAR:
-               fprintf(fd, "LITERAL_CHAR, ");break;
+               printf(fd, "LITERAL_CHAR, ");break;
           case AST_LIT_STRING:
-               fprintf(fd, "LITERAL_STRING, ");break;
+               printf(fd, "LITERAL_STRING, ");break;
           case AST_INIT_VEC_VALUES:
-               fprintf(fd, "INIT_VECTOR_VALUES, ");break;
+               printf(fd, "INIT_VECTOR_VALUES, ");break;
           case AST_KW_WORD:
-               fprintf(fd, "KW_WORD, ");break;
+               printf(fd, "KW_WORD, ");break;
           case AST_KW_BYTE:
-               fprintf(fd, "KW_BYTE, ");break;
+               printf(fd, "KW_BYTE, ");break;
           case AST_KW_BOOL:
-               fprintf(fd, "KW_BOOL, ");break;
+               printf(fd, "KW_BOOL, ");break;
           case AST_FUNCT_PARAMS:
-               fprintf(fd, "FUNCT_PARAMS, ");break;
+               printf(fd, "FUNCT_PARAMS, ");break;
           case AST_FUNCT_MORE_PARAMS:
-               fprintf(fd, "FUNCT_MOREPARAMS, ");break;
+               printf(fd, "FUNCT_MOREPARAMS, ");break;
           case AST_BLOCK:
-               fprintf(fd, "BLOCK, ");break;
+               printf(fd, "BLOCK, ");break;
           case AST_VECTOR_EQUALS:
-               fprintf(fd, "VECTOR_EQUALS, ");break;
+               printf(fd, "VECTOR_EQUALS, ");break;
           case AST_OUTP1:
-               fprintf(fd, "OUTP_1, ");break;
+               printf(fd, "OUTP_1, ");break;
           case AST_OUTP2:
-               fprintf(fd, "OUTP_2, ");break;
+               printf(fd, "OUTP_2, ");break;
           case AST_HIGHER:
-               fprintf(fd, "HIGHER, ");break;
+               printf(fd, "HIGHER, ");break;
           case AST_LOWER:
-               fprintf(fd, "LOWER, ");break;
+               printf(fd, "LOWER, ");break;
           case AST_POINTER:
-               fprintf(fd, "POINTER, ");break;
+               printf(fd, "POINTER, ");break;
           case AST_POINTER_REF:
-               fprintf(fd, "POINTERREF, ");break;
+               printf(fd, "POINTERREF, ");break;
           case AST_EXPR_W_BRACKETS:
-               fprintf(fd, "EXPR_BRACKETS, ");break;
+               printf(fd, "EXPR_BRACKETS, ");break;
           case AST_NOT:
-               fprintf(fd, "NOT, ");break;
+               printf(fd, "NOT, ");break;
           case AST_OPERATOR_AND:
-               fprintf(fd, "OP_AND, ");break;
+               printf(fd, "OP_AND, ");break;
           case AST_OPERATOR_OR:
-               fprintf(fd, "OP_OR, ");break;
+               printf(fd, "OP_OR, ");break;
           case AST_OPERATOR_LE:
-               fprintf(fd, "OP_LE, ");break;
+               printf(fd, "OP_LE, ");break;
           case AST_OPERATOR_GE:
-               fprintf(fd, "OP_GE, ");break;
+               printf(fd, "OP_GE, ");break;
           case AST_OPERATOR_EQ:
-               fprintf(fd, "OP_EQ, ");break;
+               printf(fd, "OP_EQ, ");break;
           case AST_OPERATOR_NE:
-               fprintf(fd, "OP_NE, ");break;
+               printf(fd, "OP_NE, ");break;
           case AST_TK_IDENTIFIER_VET:
-               fprintf(fd, "TKIDENTF_VEC, ");break;
+               printf(fd, "TKIDENTF_VEC, ");break;
           case AST_EXPR_W_TKIDENTIFIER:
-               fprintf(fd, "EXPR_W_TKIDENTIFIER, ");break;
+               printf(fd, "EXPR_W_TKIDENTIFIER, ");break;
           case AST_USING_PRIMARY_PARAMETER:
-               fprintf(fd, "USING_PRIMARY_PARAMETER, ");break;
+               printf(fd, "USING_PRIMARY_PARAMETER, ");break;
           case AST_USING_PARAMETERS:
-               fprintf(fd, "USING_PARAMETERS, ");break;
+               printf(fd, "USING_PARAMETERS, ");break;
           case AST_USING_PARAMETER_CONST:
-               fprintf(fd, "USING_PARAMETER_CONST, ");break;
+               printf(fd, "USING_PARAMETER_CONST, ");break;
           default: fprintf(fd, "unknown,");break; 
      }
      if (node->symbol != 0)
